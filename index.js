b7625b3d1e79b0489651fb570cb82c948ad005bc
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-08-10 21:52:52 +0530

    wrote the function to randomly select a square for mole to pop

---
+++ index.js
@@ -0,0 +1,22 @@
+//getting all the list of the squares from html into an array
+const squares = document.querySelectorAll(".square");
+const mole = document.querySelector(".mole");
+const score = document.getElementById("score");
+const time = document.getElementById("time-left");
+
+let result = 0;
+
+//creating a function to select random squares for the mole to pop
+function randomSquare(){
+    //removing the existing mole if any squares has one so that we can start fresh
+    squares.forEach((square)=>{
+        square.classList.remove('mole');
+    });
+
+    //getting random square postion 
+    let randomPosition = squares[Math.floor(Math.random()*9)];
+    //adding the mole to that particular random mole class
+    randomPosition.classList.add('mole');
+}
+
+randomSquare();

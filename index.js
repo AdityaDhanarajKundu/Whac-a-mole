6897882226f5ecff757ba70cd430785af44d1d75
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-08-11 08:17:24 +0530

    added a moveMole method on the click of the start button

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const squares = document.querySelectorAll(".square");
 const mole = document.querySelector(".mole");
 const score = document.getElementById("score");
 const time = document.getElementById("time-left");
+const start = document.getElementById("start");
 
 let result = 0;
 
@@ -19,4 +20,9 @@ function randomSquare(){
     randomPosition.classList.add('mole');
 }
 
-randomSquare();
+function moveMole(){
+    let timerId = null;
+    timerId = setInterval(randomSquare,500); //call the randomSquare method after every 500 milliseconds
+}
+
+start.addEventListener("click",moveMole);
